72016d5c2b9c45bd2d95864bcce04e8dc5a0d3ed
aleshabaranov <61497343+aleshabaranov@users.noreply.github.com>
2020-05-17 17:38:26 +0500

    Add files via upload

---
+++ css/main.js
@@ -0,0 +1,14 @@
+var button = document.querySelector('#button');
+var buttonBodyRepair = document.querySelector('#buttonBodyRepair')
+var modal = document.querySelector('#modal');
+var close = document.querySelector('#close');
+
+button.addEventListener('click', function(){
+    modal.classList.add('modal_active');
+});
+buttonBodyRepair.addEventListener('click', function(){
+    modal.classList.add('modal_active');
+});
+close.addEventListener('click', function(){
+    modal.classList.remove('modal_active');
+});
